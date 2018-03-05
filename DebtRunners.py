0bbedf4496b4f8a9d5976ed9a8240f29ade6117b
GraceBrumder <36893019+GraceBrumder@users.noreply.github.com>
2018-03-05 09:05:23 +0000

    Add files via upload

--- DebtRunners.py
+++ DebtRunners.py
@@ -1,190 +1,70 @@
-from SimpleGUICS2Pygame import simpleguics2pygame as simplegui
+######################################################################
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
 import pygame
-from Player import Player
-from Enemy import Enemy
-from Vector import Vector
-from Weapon import Pistol
+pygame.init()
+import sys
 
+#   Highlight-able menu in Pygame
 
-class Game:
-    def __init__(self, w=600, h=400):
-        self.CANVAS_WIDTH = w
-        self.CANVAS_HEIGHT = h
-        self.pointer = Vector()
-        self.initialise()
+#   1. Create a pygame.font.Font object
+#   2. Create a Surface object with the text drawn on it by calling the Font objects render() method.
+#   3. Create a Rect object from the Surface object by calling the Surface objects get_rect() method.
+#      This Rect object will have the width and height correctly set for the text that was rendered,
+#      but the top and left attributes will be 0.
+#   4. Set the position of the Rect object by changing one of its attributes.
+#   5. Blit the Surface object with the text onto the Surface object returned by pygame.display.set_mode().
+#   6. Call pygame.display.update() to make the display Surface appear on the screen.
 
-        self.wave1()
+class Menu_item:
 
-        frame = simplegui.create_frame('Debt Runners', self.CANVAS_WIDTH, self.CANVAS_HEIGHT)
-        frame.set_draw_handler(self.draw)
-        frame.set_keydown_handler(self.kbd.keyDown)
-        frame.set_keyup_handler(self.kbd.keyUp)
-        frame.set_mouseclick_handler(self.click)
-        frame.start()
+    hovered = False
 
-    def initialise(self):
-        self.mouse = Mouse()
-        self.kbd = Keyboard()
-        self.player = Player(Vector(self.CANVAS_WIDTH / 2, self.CANVAS_HEIGHT / 4 * 3))
-        self.move = Movement(self.player, self.kbd)
-        self.enemies = []
+    def __init__(self, text, pos):
+        self.text = text
+        self.pos = pos
+        self.rend = menu_font.render(self.text, False, self.get_color())
+        self.rect = self.rend.get_rect()
+        self.rect.topleft = self.pos
+        screen.blit(self.rend, self.rect)
 
-    def wave1(self):
-        for e in range(3):
-            # Assigns the enemies different positions, health and a new weapon
-            self.enemies.append(Enemy(Vector(self.CANVAS_WIDTH / 4 * (e + 1), self.CANVAS_HEIGHT / 4), 10, Pistol()))
+    def draw(self):
+        self.rend = menu_font.render(self.text, False, self.get_color())
+        screen.blit(self.rend, self.rect)
 
-    def draw(self, canvas):
-        # UPDATE CHARS
-        self.move.update()
-        self.player.update(self.mouse.pos.copy())
-        self.mouse.update()
+    def get_color(self):
+        if self.hovered:
+            return (100, 100, 100)
+        else:
+            return (255, 255, 255)
 
-        # Draw and update enemies
-        for enemy in self.enemies:
-            # temp shooting place
-            enemy.update(self.player.pos.copy())
-            enemy.draw(canvas)
-            enemy.weapon.addAttack(self.player.pos.copy(), enemy.weapon.pos.copy())
+screen = pygame.display.set_mode((640, 480))
 
-        # Bullet OoB(Out of Bounds) check
-        for bullet in self.player.weapon.attack:
-            if bullet.pos.y > self.CANVAS_HEIGHT - 20 or bullet.pos.y < 20 or bullet.pos.x > self.CANVAS_WIDTH - 20 or bullet.pos.x < 20:
-                self.player.weapon.removeAttack(bullet)
+screen.fill((100, 50, 150)) #Background colour
 
-        for enemy in self.enemies:
-            for bullet in enemy.weapon.attack:
-                if bullet.pos.y > self.CANVAS_HEIGHT - 20 or bullet.pos.y < 20 or bullet.pos.x > self.CANVAS_WIDTH - 20 or bullet.pos.x < 20:
-                    enemy.weapon.removeAttack(bullet)
+menu_font = pygame.font.Font(None, 40)
 
-        # This is for checking to see if the bullet is within the enemies hitbox.
-        for bullet in self.player.weapon.attack:
-            for enemyIndex in range(len(self.enemies)):
-                if (bullet.pos.x < (self.enemies[enemyIndex].pos.x + self.enemies[enemyIndex].size)) and (
-                        bullet.pos.x > (self.enemies[enemyIndex].pos.x - self.enemies[enemyIndex].size)) and (
-                        bullet.pos.y < (self.enemies[enemyIndex].pos.y + self.enemies[enemyIndex].size)) and (
-                        bullet.pos.y > (self.enemies[enemyIndex].pos.y - self.enemies[enemyIndex].size)):
+menu_items = [Menu_item("Play Game", (140, 105)),
+              Menu_item("Instructions", (140, 155)),
+              Menu_item("Exit", (140, 205))]
 
-                    # Subtracting damamge from enemies health
-                    self.enemies[enemyIndex].damage(self.player.weapon.damage)
-
-                    # Removing bullet, so that it does not go through the enemy
-                    self.player.weapon.removeAttack(bullet)
-
-                    # Removing enemies from list enemy list
-                    if self.killCheck(self.enemies[enemyIndex]):
-                        break
-                    print('Enemy Hit!')
-
-        for enemy in self.enemies:
-            for bullet in enemy.weapon.attack:
-                if (bullet.pos.x < (self.player.pos.x + self.player.size)) and (
-                        bullet.pos.x > (self.player.pos.x - self.player.size)) and (
-                        bullet.pos.y < (self.player.pos.y + self.player.size)) and (
-                        bullet.pos.y > (self.player.pos.y - self.player.size)):
-
-
-                    # Decreasing player health when bullet lands
-                    self.player.damage(enemy.weapon.damage)
-                    print(self.player.health)
-
-                    # Removing the bullet so that is does not go though the enemy
-                    self.player.damage(enemy.weapon.damage)
-
-                    enemy.weapon.removeAttack(bullet)
-                    print("Player hit!")
-
-
-        # DRAW CHARS HERE
-        self.player.draw(canvas)
-
-        # Debug print
-        # print(self.player.pos)
-        # print(self.player.weapon)
-
-    def click(self, pos):
-        self.player.weapon.addAttack(self.mouse.pos.copy(), self.player.weapon.pos.copy())
-
-    def killCheck(self, enemy):
-        kill = False
-        if enemy.health <= 0:
-            kill = True
-            self.enemies.remove(enemy)
-        return kill
-
-
-class Mouse:
-    def __init__(self):
-        self.pos = Vector()
-
-    def update(self):
-        self.pos = Vector(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]).subtract(Vector(250, 25))
-
-
-class Keyboard:
-    def __init__(self):
-        self.right = False
-        self.left = False
-        self.up = False
-        self.down = False
-
-    def keyDown(self, key):
-        if key == simplegui.KEY_MAP['D']:
-            self.right = True
-
-        if key == simplegui.KEY_MAP['A']:
-            self.left = True
-
-        if key == simplegui.KEY_MAP['W']:
-            self.up = True
-
-        if key == simplegui.KEY_MAP['S']:
-            self.down = True
-
-    def keyUp(self, key):
-        if key == simplegui.KEY_MAP['D']:
-            self.right = False
-
-        if key == simplegui.KEY_MAP['A']:
-            self.left = False
-
-        if key == simplegui.KEY_MAP['W']:
-            self.up = False
-
-        if key == simplegui.KEY_MAP['S']:
-            self.down = False
-
-
-class Movement():  # solver
-    def __init__(self, player, keyboard):
-        self.player = player
-        self.keyboard = keyboard
-
-    def update(self):
-        vel = Vector()
-        if self.keyboard.up:
-            vel = vel.add(Vector(0, -1))
-
-        if self.keyboard.right:
-            vel = vel.add(Vector(1, 0))
-
-        if self.keyboard.left:
-            vel = vel.add(Vector(-1, 0))
-
-        if self.keyboard.down:
-            vel = vel.add(Vector(0, 1))
-
-        if self.keyboard.up or self.keyboard.down or self.keyboard.left or self.keyboard.right:
-            if not vel == Vector():
-                self.player.vel.add(vel.normalize().multiply(self.player.speed))
-
-
-class Interaction():
-    def __init__(self, ):
-        pass
-
-    def update(self):
-        pass
-
-
-game = Game()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        for menu_item in menu_items:
+            if menu_item.rect.collidepoint(pygame.mouse.get_pos()):
+                menu_item.hovered = True
+                if menu_item.text == "Play Game" and event.type == pygame.MOUSEBUTTONDOWN:
+                    print ("Play Game")
+                if menu_item.text == "Exit" and event.type == pygame.MOUSEBUTTONDOWN:
+                    sys.exit()
+            else:
+                menu_item.hovered = False
+            menu_item.draw()
+        pygame.display.update()
+#
+windowSurfaceObj.blit(catSurfaceObj, (mousex, mousey))
